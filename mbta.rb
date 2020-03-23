b2932aedf86c289801bc03b122dbed069113c268
huda <you@example.com>
2020-03-23 23:24:53 +0300

    solved HW

--- mbta.rb
+++ mbta.rb
@@ -6,7 +6,6 @@ def stops_between_stations(start_line, start_station, end_line, end_station)
         Green: ["Government Center", "Park Street", "Boylston", "Arlington", "Copley", "Hynes", "Kenmore" ],
         Orange: [ "North Station", "Haymarket", "Park Street", "State", "Downtown Crossing", "Chinatown", "Back Bay", "Forest Hills"]
       }
-      
       start_station = lines [start_line.to_sym].index(start_station)
       end_station = lines [end_line.to_sym].index(end_station)
       
@@ -23,7 +22,6 @@ def stops_between_stations(start_line, start_station, end_line, end_station)
             stops = stop1 + stop2
             return stops
             end
-            # puts stops_between_stations("Red", "Alewife", "Red", "Alewife")
-            # puts stops_between_stations("Red", "Alewife", "Red", "South Station")
-            # puts stops_between_stations("Red", "South Station", "Green", "Kenmore") 
-            # puts stops_between_stations("Green", "Park Street", "Red", "Alewife")
+            puts stops_between_stations("Red", "Alewife", "Red", "Alewife")
+            puts stops_between_stations("Red", "Alewife", "Red", "South Station")
+            puts stops_between_stations("Red", "South Station", "Green", "Kenmore") 
