be9d34c1a35d266d0151761094ae55c703120176
Sara-Kuddah <sara-kuddah@hotmail.com>
2020-03-24 00:55:33 +0300

    start create 3 sub lines

--- mbta.rb
+++ mbta.rb
@@ -1,5 +1,9 @@
 
 # your solution here
 def stops_between_stations
+ red_line=["South Station","Park Street","Kendall","Central","Harvard","Porter","Davis","Alewife"]
+ green_line=["Government Center","Park Street","Boylston","Arlington","Copley","Hynes","Kenmore"]
+ orenge_line=["North Station","Haymarket","Park Street","State","Downtown Crossing","Chinatown","Back Bay","Back Bay"] 
+   
 
 end
