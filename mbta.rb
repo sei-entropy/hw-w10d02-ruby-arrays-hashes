ae2b2efb9cb5260c0c66fd8f62a46ec039965ad0
Sara-Kuddah <sara-kuddah@hotmail.com>
2020-03-24 00:58:35 +0300

    updating method arrguments

--- mbta.rb
+++ mbta.rb
@@ -1,9 +1,9 @@
 
 # your solution here
-def stops_between_stations
+def stops_between_stations(start_line,start_station,end_line,end_station)
  red_line=["South Station","Park Street","Kendall","Central","Harvard","Porter","Davis","Alewife"]
  green_line=["Government Center","Park Street","Boylston","Arlington","Copley","Hynes","Kenmore"]
  orenge_line=["North Station","Haymarket","Park Street","State","Downtown Crossing","Chinatown","Back Bay","Back Bay"] 
-   
+
 
 end
